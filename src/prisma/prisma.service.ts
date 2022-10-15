3e4b5bf2e1f4c42aa9ffab0c2f912d54dc3ce919
Mizanur Rahman <mizanur.rahman@navana.com>
2022-10-15 12:44:39 +0600

    Mongo db connected and user entry inserted

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,9 +7,13 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: 'mongodb://localhost/mydb',
+          url: 'mongodb+srv://bongoweb:Nick$300^12@cluster0.t4q5myy.mongodb.net/nesttest',
         },
       },
     });
   }
 }
+
+// mongodb://USERNAME:PASSWORD@HOST/DATABASE?ssl=true&connectTimeoutMS=5000&maxPoolSize=50
+// DATABASE_URL="mongodb+srv://test:test@cluster0.ns1yp.mongodb.net/myFirstDatabase"
+// url: 'mongodb+srv://bongoweb:Nick$300^12@cluster0.t4q5myy.mongodb.net/nesttest',
