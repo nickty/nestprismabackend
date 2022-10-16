5d03d6adbf22d7585ee46982f3ceebf9fa701d1d
Mizanur Rahman <mizanur.rahman@navana.com>
2022-10-16 19:56:59 +0600

    Strategy for authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStragery } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStragery],
 })
 export class AuthModule {}
