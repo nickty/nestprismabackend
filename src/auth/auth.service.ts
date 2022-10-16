b2306619687bbc5f6c389c600f60b60932e529dc
Mizanur Rahman <mizanur.rahman@navana.com>
2022-10-17 05:15:23 +0600

    e2e test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
